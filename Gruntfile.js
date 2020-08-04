9df4b6aaebe9f24de270e0b0b4173ca8e5a73cc2
Le Khanh Toan <toanlk@bestprice.vn>
2020-08-04 08:53:11 +0700

    Update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,46 @@
 module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        // minify javascript files
         uglify: {
             build: {
                 files: {
-                    'build/js/main.min.js': ['src/js/jquery.min.js', 'src/js/main.js']
+                    'built/js/main.min.js': ['src/js/jquery.min.js', 'src/js/main.js']
                 }
             }
-        }
+        },
+
+        // minify css files
+        cssmin: {
+            target: {
+                files: {
+                    'built/css/main.min.css' : ['src/css/main.css'],
+                }
+            }
+        },
+
+        // watch file change
+        watch: {
+            options: {
+                livereload: true,
+            },
+            css: {
+                files: ['src/css/*.css'],
+                tasks: []
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: []
+            },
+            html: {
+                files: ['*.html'],
+                tasks: []
+            },
+        },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'cssmin']);
 }
